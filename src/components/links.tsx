7b61aa504e7d16787be82eb58df149d6c3f74aa8
kohako_dev <kohako.tech@gmail.com>
2025-02-10 04:02:17 -0300

    feat: creat navbar links to footer

---
+++ src/components/links.tsx
@@ -0,0 +1,12 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "KWK - Astros",
+  links: {
+    github: "https://github.com/dev-kohako",
+    youtube: "https://www.youtube.com/@dev_kohako",
+    instagram: "https://www.instagram.com/kohako.dev/",
+    linkedin: "www.linkedin.com/in/josephkawe",
+    bento: "https://bento.me/kohako",
+  },
+};
